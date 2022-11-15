8646ad7ab7c9b4b96f4b910ba8b6c36bc3e19820
GunnarIng <thomas.g.ingvarsson@gmail.com>
2022-11-15 11:17:21 +0100

    fixat skiten

---
+++ README.md
@@ -0,0 +1,8 @@
+# Timeout Funktioner med färger
+
+Ett litet projekt som demar  "setTimout" och "setInterval" i JS 
+
+här kan du testa koden 
+
+[Demo länk:](https://gunnaring.github.io/code-with-timefunctions/)
+
--- main.js
+++ main.js
@@ -1,50 +1,48 @@
-window.addEventListener("DOMContentLoaded", main)
+window.addEventListener("DOMContentLoaded", main);
 
 let timeoutRef;
 
-function main(){
-    // Start på programmet
-    // main är en callback funktion
+function main() {
+  // Start på programmet
+  // main är en callback funktion
 
-    timeoutRef = setTimeout(changeBackgroundColor, 4000)
-    addEventListeners()
-   
+  timeoutRef = setTimeout(changeBackgroundColor, 4000);
+  addEventListeners();
 }
 
-function changeBackgroundColor(){
-    document.body.style.background = "grey"
+function changeBackgroundColor() {
+  document.body.style.background = getRandomColor();
 }
 
-function addEventListeners(){
- const cancelTimeoutBtn = document.getElementById ("cancel")
- cancelTimeoutBtn.addEventListener("click", cancelBackgroundChange)
+function addEventListeners() {
+  const cancelTimeoutBtn = document.getElementById("cancel");
+  cancelTimeoutBtn.addEventListener("click", cancelBackgroundChange);
 
- const startIntervalButton = document.getElementById("start-interval")
- startIntervalButton.addEventListener("click", startBackgroundChangeInterval)
+  const startIntervalButton = document.getElementById("start-interval");
+  startIntervalButton.addEventListener("click", startBackgroundChangeInterval);
 }
 
 
-function cancelBackgroundChange(){
-    clearTimeout(timeoutRef)
+function cancelBackgroundChange() {
+  clearTimeout(timeoutRef);
 }
 
-function startBackgroundChangeInterval(){
-    setInterval(changeBackgroundColor, 1000)
+function startBackgroundChangeInterval() {
+  setInterval(changeBackgroundColor, 1);
 }
 
 function getRandomColor() {
-   const value =  Math.random()
+  const value = Math.random();
 
-   if(value > 0.2) {
-    return "blue"
-   } else if (value < 0.4) {
-    return "red"            // Return gör att man hoppr ur funktionen och skickar värdet i detta fall red.
-   } else if (Value < 0.6) {
-    return "green"
-   } else if (value < 0.8) {
-    return "yellow"
-   } else {
-    return "pink"
-   }
-
+  if (value < 0.2) {
+    return "blue";
+  } else if (value < 0.4) {
+    return "red"; 
+  } else if (value < 0.6) {
+    return "green";
+  } else if (value < 0.8) {
+    return "yellow";
+  } else {
+    return "pink";
+  }
 }
